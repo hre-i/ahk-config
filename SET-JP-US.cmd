66e2835c324ddd98ebc05e64516cad6438a02925
IMAI Hiroei <hre.bz.i@gmail.com>
2020-10-22 08:01:43 +0900

    Update: 設定スクリプトの更新

--- SET-JP-US.cmd
+++ SET-JP-US.cmd
@@ -2,12 +2,16 @@ rem HHK-JP
 
 rem copy %~dp0\AutoHotkey.ahk %~dp0\..\..\..\Documents\Autohotkey.ahk
 
+copy %~dp0\AutoHotkey.ahk C:\Users\hre\Documents\Autohotkey.ahk
+
 reg import %~dp0\KEYMAP-JP-US.reg
 reg import %~dp0\KBD-US.reg
 
 echo off
 
-echo Type [Enter] to log off.
+echo ##########################################
+echo          Type [Enter] to reboot
+echo ##########################################
 
 pause
 
